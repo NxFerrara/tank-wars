e67f54ffbfd7da1a9aa348a26f02ecd4bb806c0c
kc34251 <kc4433@nyu.edu>
2024-12-12 22:08:45 -0500

    Menu

--- src/main/java/com/tankwars/Main.java
+++ src/main/java/com/tankwars/Main.java
@@ -1,21 +1,28 @@
 package com.tankwars;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import com.tankwars.ui.GameScene;
 import com.tankwars.game.GameManager;
+import com.tankwars.ui.MenuScene;
 
 public class Main extends Application {
     private GameManager gameManager;
 
     @Override
     public void start(Stage primaryStage) {
-        GameScene gameScene = new GameScene(null);
-        gameManager = new GameManager(gameScene);
-        gameScene.setGameManager(gameManager);
-        
+        MenuScene menuManager = new MenuScene(gameManager);
+        // Set the initial scene to the menu
+        Scene menuScene = menuManager.createMenuScene(primaryStage);
         primaryStage.setTitle("Tank Wars");
-        primaryStage.setScene(gameScene);
+        primaryStage.setScene(menuScene);
         primaryStage.show();
     }
 
